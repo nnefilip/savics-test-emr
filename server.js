0a005d546b2cfc27bb2b8c3a3f988573a5f82a41
Philippe DELL <philippe@DELL>
2021-08-16 12:01:12 +0100

    Comment our code

--- server.js
+++ server.js
@@ -4,20 +4,24 @@ var app = express();
 var db = require("./database");
 var bodyParser = require("body-parser");
 
+// defined the port based on specificattions
 var HTTP_PORT = 8484;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.listen(HTTP_PORT, () => {
+    // all running
     console.log("Server is running on port %PORT%".replace("%PORT%", HTTP_PORT));
 });
 
+// default URL to be sure all is ok
 app.get("/", (req, res, next) => {
     console.log("Working URL");
     res.json({"message":"OK"});
 });
 
+// GET all patients files
 app.get("/api/1.0/emr", (req, res, next) => {
     var sql = "SELECT * from patients";
     var params = [];
@@ -34,6 +38,7 @@ app.get("/api/1.0/emr", (req, res, next) => {
     });
 });
 
+// create a new patient file
 app.post("/api/1.0/emr", (req, res, next) => {
     var errors = [];
     if (!req.body.firstname) {
@@ -57,11 +62,14 @@ app.post("/api/1.0/emr", (req, res, next) => {
     if (!req.body.diabetic) {
         errors.push("You have to define a diabetic status");
     }
+    // is all the fields defined or not
     if (errors.length) {
+        // send message to ask to define elmpty fields
         res.status(400).json({"error":errors.join(",")});
         return;
     }
 
+    // structured data before created a new patient
     var data = {
         firstname: req.body.firstname,
         lastname: req.body.lastname,
@@ -87,6 +95,7 @@ app.post("/api/1.0/emr", (req, res, next) => {
     });
 })
 
+// capture all other URL
 app.use(function (req, res) {
     res.status(404);
 });
